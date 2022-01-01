fb1b71f801d8658f36697acd2d70b5427334c2fe
varinder Ghotra <ghotra3v@gmail.com>
2021-12-31 22:48:11 -0600

    comments

--- multiplication.py
+++ multiplication.py
@@ -1,12 +1,17 @@
+''' prints out the multoplication with wrong wrong index
+    one function will write the multiplication table 
+    second function will find which index is wrong'''
+
+
 import random
 
-
+# function to print the multiplication table 
 def rohanMulitplication(number):
     wrong = random.randint(0, 9)
     table = [i * number for i in range(1, 11)]
     table[wrong] = table[wrong] + random.randint(0, 10)
     return table
-
+# function to print the wrong index of table 
 
 def isCorrect(table, number):
     for i in range(1, 11):
